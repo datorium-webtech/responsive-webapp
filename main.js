dc442326ee101e2132cf39bab897bec61a4d6203
CornyBot <tomass.vite@gmail.com>
2020-07-25 19:40:07 +0300

    July 25: jQuery, Pt. 2 and polishing

--- main.js
+++ main.js
@@ -19,5 +19,22 @@ function toggleMenu() {
 // Check if jQuery is working
 console.log("jQuery version: " + $().jquery);
 
-// Select the testElement and change html to "XYZ"
-$("#testElement").html("XYZ");
+// Select the blue "+" button and add click event.
+$(".expand-product-button").on("click", function() {
+
+    // .parent() - element upwards
+    // .children("???") - element downwards (optional selector)
+    $(this).parent().parent().parent().children(":not(:first-child)").slideToggle(200);
+
+    // Write a if statement, that changes button between
+    // "add_circle" and "remove_circle"
+    if ($(this).html() === "add_circle") {
+        $(this).html("remove_circle");
+    } else {
+        $(this).html("add_circle");
+    }
+});
+
+// Collapse all products initially for mobile
+// Select all details for products (gray divs) and hide them with .toggle();
+$(".product > div:not(:first-child)").toggle();
